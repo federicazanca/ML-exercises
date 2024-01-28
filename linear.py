a181aa5ad9a20ac5f02a8e5a0f7e1613cde4934c
federica <federicazanca8@gmail.com>
2024-01-28 00:34:11 +0000

    come back here if I destroy everything

--- linear.py
+++ linear.py
@@ -2,27 +2,49 @@ import numpy as np
 
 def linear_regression_gradient(x_train: np.ndarray, y_train: np.ndarray, params: np.ndarray) -> np.ndarray:
     '''
+    x_train: n*m matrix of features, n samples and m features 
+    y_train: array of targets, n samples
+    params: array of linear regression params, m + 1 entries [b, w1, w2, ..., wm]
     Return cost function gradient for linear regression in params
     '''
-    #cost = 1/2m * sum((fx-y)^2) = 1/2m * sum((wx+b-y)^2)
-    #dw = 1/m * sum((wx+b-y)x)
-    #db = 1/m * sum((wx+b-y))
-    n = x_train.shape[0]
-    dw = 0
-    for i in range(n):
-        dw += x_train[i]*(x_train[i]*params[0]+params[1]-y_train[i])
-    dw /= n
+    # for linear regression, the cost function is:
+    # cost = 1/2n * sum((fx-y)^2) = 1/2n * sum((wx+b-y)^2)
+    # here we compute algebrically the gradient of the cost
+    # dw = 1/n * sum((wx+b-y)x)
+    # db = 1/n * sum((wx+b-y))
+
+    n, m = x_train.shape
+    assert y_train.shape == (n,)
+    assert params.shape == (m+1,)
+    b, *w = params
+
+    # partial derivatives w.r.t. w
+    dw = [0.0]*m
+    for j in range(m):
+        for i in range(n):
+            dw[j] += x_train[i][j]*(np.dot(x_train[i],w)+b-y_train[i])
+        dw[j] /= n
+
+    # partial derivate w.r.t. b
     db = 0
     for i in range(n):
-        db += (x_train[i]*params[0]+params[1]-y_train[i])
+        db += np.dot(x_train[i], w)+b-y_train[i]
     db /= n
-    return np.array([dw,db])
+
+    # gradient
+    return np.array([db, *dw])
 
 
 def train(x_train: np.ndarray, y_train: np.ndarray, params: np.ndarray, rate: float, calc_gradient) -> np.ndarray:
     '''
     Return new params after one iteration of steepest descent
+    x_train: n*m matrix of features, n samples and m features 
+    y_train: array of targets, n samples
+    params: array of linear regression params, m + 1 entries [b, w1, w2, ..., wm]
+    rate: number, indicates the learning rate
+    calc_gradient: a function to calculate the gradient (different for each model)
     '''
+
     m = params.shape[0]
     gradient = calc_gradient(x_train, y_train, params)
     updated_params = np.array(params)
@@ -31,15 +53,15 @@ def train(x_train: np.ndarray, y_train: np.ndarray, params: np.ndarray, rate: fl
     return updated_params
 
 if __name__ == '__main__':
-    x_train = np.array([0.0, 0.5, 1.0])
+    x_train = np.array([[0.0], [0.5], [1.0]])
     y_train = np.array([1.0, 3.0, 5.0])
     params = np.array([0.0, 0.0])
 
-    before_train = params[0] * 2.0 + params[1]
+    before_train = params[1] * 2.0 + params[0]
     print(str(before_train))
 
     for i in range(10000):
-        params = train(x_train, y_train, params, 5, linear_regression_gradient)
+        params = train(x_train, y_train, params, 0.1, linear_regression_gradient)
 
-    after_train = params[0] * 2.0 + params[1]
+    after_train = params[1] * 2.0 + params[0]
     print(str(after_train))
